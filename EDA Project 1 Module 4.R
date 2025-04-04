963753974541c1a1146804a7416afd8cc7c7f3e9
RMendoza-Marriott <roddy.mendoza@icloud.com>
2025-04-04 09:21:44 -0500

    generating plot 1: global active power and saving png plot.

--- EDA Project 1 Module 4.R
+++ EDA Project 1 Module 4.R
@@ -12,6 +12,7 @@
 ### setting the working directory ###
 
 setwd("~/Coursera/2025/Modulo 4/week 1 - module 1/Project 1 module 4")
+dev.new(noRStudioGD = TRUE)
 
 ### hpc: house power consumption 
 
@@ -31,3 +32,13 @@ shpc <- subset(hpc, Date >= "2007-02-01" & Date <= "2007-02-02")
 
 datetime <- paste(as.Date(shpc$Date), shpc$Time)
 shpc$Datetime <- as.POSIXct(datetime)
+
+
+### Generating plot 1 : Global active power ----
+
+
+hist(shpc$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+
+
