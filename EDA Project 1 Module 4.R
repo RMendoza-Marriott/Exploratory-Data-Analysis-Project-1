abc3edbad0192b01ec8da7c8d85453bc8bd94fe6
RMendoza-Marriott <roddy.mendoza@icloud.com>
2025-04-04 10:16:19 -0500

    Generating plot 3: Energy sub metering

--- EDA Project 1 Module 4.R
+++ EDA Project 1 Module 4.R
@@ -47,6 +47,22 @@ plot(shpc$Global_active_power~shpc$Datetime,type="l", ylab="Global Active Power 
 
 
 
+### Generating plot 3 : Energy sub metering ----
+
+
+with(shpc, {
+  plot(Sub_metering_1~Datetime, type="l", ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+
+
+
+
+
 
 
 
